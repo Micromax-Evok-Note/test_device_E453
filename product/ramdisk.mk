3451d023e636b419a36cee98ae276bed34fda869
Sooraj S Prakash <soorajsprakash007@gmail.com>
2020-06-24 12:40:39 +0530

    added some product stuffs

---
+++ product/ramdisk.mk
@@ -0,0 +1,25 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.usb.rc:root/factory_init.usb.rc \
+    $(LOCAL_PATH)/rootdir/fstab.charger:root/fstab.charger \
+    $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+    $(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
+    $(LOCAL_PATH)/rootdir/init.common_svc.rc:root/init.common_svc.rc \
+    $(LOCAL_PATH)/rootdir/init.hqfactory.rc:root/init.hqfactory.rc \
+    $(LOCAL_PATH)/rootdir/init.mal.rc:root/init.mal.rc \
+	$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+    $(LOCAL_PATH)/rootdir/init.trace.rc:root/init.trace.rc \
+    $(LOCAL_PATH)/rootdir/init.volte.rc:root/init.volte.rc \
+    $(LOCAL_PATH)/rootdir/init.xlog.rc:root/init.xlog.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc
